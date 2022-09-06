eabda07ca1f4872434448eaada92ce7ef58da294
MukeshkumarA <mukeshkumar.mk1911@gmail.com>
2022-09-06 07:15:01 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+function openForm1()
+{
+	document.getElementById("myForm").style.display = "block";
+	document.getElementById("overlay").style.display = "block";
+
+}
+
+function closeForm1()
+{
+	document.getElementById("myForm").style.display= "none";
+	document.getElementById("overlay").style.display = "none";
+	
+}
+
+
+function openForm2()
+{
+	document.getElementById("Form").style.display = "block";
+	document.getElementById("overlay").style.display = "block";
+}
+function closeForm2()
+{
+	document.getElementById("Form").style.display= "none";
+	document.getElementById("overlay").style.display = "none";
+	
+}
